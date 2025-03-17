bcca6d0b988c7603c3e697bff629217d7ab786f1
hubertwojcik <55180668+hubertwojcik@users.noreply.github.com>
2025-03-17 16:47:02 +0100

    Time: 19 ms (31.41%) | Memory: 64.8 MB (5.38%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.ts
+++ 347-top-k-frequent-elements/top-k-frequent-elements.ts
@@ -1,27 +1,27 @@
 function topKFrequent(nums: number[], k: number): number[] {
-    const counter = {}
+    const counter  = {}
     const freq = Array.from({length:nums.length + 1},()=>[])
 
     for (let num of nums){
         counter[num] = (counter[num] || 0) + 1
-    }
-    for (let key of Object.keys(counter)){
-        freq[counter[key]].push(key)
+    }   
+    for (let key in counter){        
+        freq[counter[key]].push(Number(key))        
     }
 
-    const result: number[] = [];
+    const result = []
     
-    for (let i = freq.length-1; i >=0 ; i --){
-        
-       for (let num of freq[i]) {
-            result.push(Number(num));
-            if (result.length === k) return result;
-        }
-
-        if(result.length ==k){
-            return result
-        }
+    for (let i = nums.length; i >= 0; i--){
+            for (let num of freq[i]){
+                result.push(num)
+                if(result.length == k){
+                    return result
+                }
+            }
     }
 
-    return result
+    
+
+  
+
 };
