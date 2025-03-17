9641bc603deda3777a72dbb8fab5473a3850ea19
hubertwojcik <55180668+hubertwojcik@users.noreply.github.com>
2025-03-17 13:13:23 +0100

    Time: 19 ms (87.35%) | Memory: 57.7 MB (45.26%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.ts
@@ -0,0 +1,25 @@
+function longestPalindrome(s: string): string {
+    if (s.length < 2) return s;
+
+    let start = 0, maxLength = 0;
+
+    function expandAroundCenter(left: number, right: number) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            left--;
+            right++;
+        }
+        
+        const length = right - left - 1;
+        if (length > maxLength) {
+            start = left + 1;
+            maxLength = length;
+        }
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        expandAroundCenter(i, i);   
+        expandAroundCenter(i, i + 1); 
+    }
+
+    return s.substring(start, start + maxLength);
+}
