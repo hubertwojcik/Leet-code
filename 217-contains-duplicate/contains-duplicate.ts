d15f7b868a8e71dbc17b0739251aa2ac416f4f5b
hubertwojcik <55180668+hubertwojcik@users.noreply.github.com>
2025-03-17 13:49:49 +0100

    Time: 17 ms (27.10%) | Memory: 65.7 MB (48.16%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.ts
+++ 217-contains-duplicate/contains-duplicate.ts
@@ -1,21 +1,3 @@
 function containsDuplicate(nums: number[]): boolean {
-    let numsSet = new Set<number>()
-
-    for (let num of nums){
-        if(numsSet.has(num)){
-            return true
-        }
-        numsSet.add(num)
-    }
-    return false
-    // let counter = {}
-
-    // for (let num of nums){    
-    //     if(counter[num] == 1){
-    //         return true
-    //     }
-    //     counter[num] = 1
-    // }
-    
-    // return false
+    return new Set(nums).size !== nums.length
 };
