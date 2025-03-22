4e27d0fca5e91f31bbd5daf3c4ee2c04ebaa31b2
hubertwojcik <55180668+hubertwojcik@users.noreply.github.com>
2025-03-22 09:35:52 +0100

    Time: 0 ms (100.00%) | Memory: 17.7 MB (96.32%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,22 +1,18 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isValid(self, s: str) -> bool:
         stack = []
         close_to_open = {
             ")":"(",
-            "}":"{",
-            "]":"["
+            "]":"[",
+            "}":"{"
         }
+
         for c in s:
             if c in close_to_open:
-                if stack and close_to_open[c] == stack[-1]:
+                if stack and stack[-1] == close_to_open[c]:
                     stack.pop()
                 else:
                     return False
             else:
                 stack.append(c)
         return True if not stack else False
-        
