a0f9f348acb9549e954ce6ec368795dcab1990a8
hubertwojcik <55180668+hubertwojcik@users.noreply.github.com>
2025-06-16 19:57:16 +0200

    File updated at 16.06.2025, 19:57:16

--- 183-CustomersWhoNeverOrder/183-CustomersWhoNeverOrder.py
+++ 183-CustomersWhoNeverOrder/183-CustomersWhoNeverOrder.py
@@ -1,8 +1,6 @@
-# Last updated: 16.06.2025, 19:57:00
+# Last updated: 16.06.2025, 19:57:16
 import pandas as pd
 
-def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-    all_data = pd.merge(customers, orders, how='left', left_on='id', right_on='customerId')
-    
-    no_orders = all_data[all_data['customerId'].isna()]
-    return no_orders[['name']].rename(columns={'name': 'Customers'})
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    mask = (products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')
+    return products[mask][['product_id']]
